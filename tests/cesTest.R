136455e856c19898cc1e56892bb87ee9dee3720f
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-05 14:08:48 +0000

    added "print" method for objects of class "cesEst"
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1263 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- tests/cesTest.R
+++ tests/cesTest.R
@@ -10,13 +10,15 @@ germanFarms$qVarInput <- germanFarms$vVarInput / germanFarms$pVarInput
 
 ## CES: Land & Labor
 cesLandLabor <- cesEst( "qOutput", c( "land", "qLabor" ), germanFarms )
-print( cesLandLabor ) 
+print.default( cesLandLabor ) 
+print( cesLandLabor )
 coef( cesLandLabor ) 
 vcov( cesLandLabor ) 
 
 # variable returns to scale
 cesLandLaborVrs <- cesEst( "qOutput", c( "land", "qLabor" ), germanFarms,
    vrs = TRUE )
+print.default( cesLandLaborVrs )
 print( cesLandLaborVrs )
 coef( cesLandLaborVrs )
 vcov( cesLandLaborVrs )
@@ -24,6 +26,7 @@ vcov( cesLandLaborVrs )
 # using the BFGS optimization method
 cesLandLaborBfgs <- cesEst( "qOutput", c( "land", "qLabor" ), germanFarms,
    method = "BFGS" )
+print.default( cesLandLaborBfgs )
 print( cesLandLaborBfgs )
 coef( cesLandLaborBfgs )
 vcov( cesLandLaborBfgs )
@@ -32,6 +35,7 @@ vcov( cesLandLaborBfgs )
 cesLandLaborBfgsCon <- cesEst( "qOutput", c( "land", "qLabor" ),
    germanFarms, method = "L-BFGS-B", lower = c( -Inf, 0, -Inf ),
    upper = c( Inf, 1, Inf ) )
+print.default( cesLandLaborBfgsCon )
 print( cesLandLaborBfgsCon )
 coef( cesLandLaborBfgsCon )
 vcov( cesLandLaborBfgsCon )
@@ -39,20 +43,24 @@ vcov( cesLandLaborBfgsCon )
 
 ## CES: Land & Intermediate Inputs
 cesLandInt <- cesEst( "qOutput", c( "land", "qVarInput" ), germanFarms )
+print.default( cesLandInt )
 print( cesLandInt )
 
 # variable returns to scale
 cesLandIntVrs <- cesEst( "qOutput", c( "land", "qVarInput" ), germanFarms,
    vrs = TRUE )
+print.default( cesLandIntVrs )
 print( cesLandIntVrs )
 
 # using the BFGS optimization method
 cesLandIntBfgs <- cesEst( "qOutput", c( "land", "qVarInput" ), germanFarms,
    method = "BFGS" )
+print.default( cesLandIntBfgs )
 print( cesLandIntBfgs )
 
 # using the L-BFGS-B optimization method with constrained alpha
 cesLandIntBfgsCon <- cesEst( "qOutput", c( "land", "qVarInput" ), germanFarms,
    method = "L-BFGS-B", lower = c( -Inf, 0, -Inf ),
    upper = c( Inf, 1, Inf ) )
+print.default( cesLandIntBfgsCon )
 print( cesLandIntBfgsCon )
