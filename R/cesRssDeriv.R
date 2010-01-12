6de9f0d7e2b0178d03b1051f8790fe08bcbceca4
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-12 16:32:46 +0000

    cesRssDeriv() now uses cesDerivCoef to calculate the derivatives
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1303 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesRssDeriv.R
+++ R/cesRssDeriv.R
@@ -1,33 +1,13 @@
 cesRssDeriv <- function( par, data ) {
    result <- par
-   gamma <- par[ "gamma" ]
-   delta <- par[ "delta" ]
-   rho <- par[ "rho" ]
-   if( "phi" %in% names( par ) ) {
-      phi <- par[ "phi" ]
-   } else {
-      phi <- 1
-   }
+
    yHat <- cesCalc( xNames = names( data )[ -1 ], data = data, coef = par )
    resid <- data$y - yHat
-   result[ "gamma" ] <- sum( - 2 * resid *
-      ( ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho ) ) )
-   result[ "delta" ] <- sum( -2 * resid * ( gamma * ( -phi / rho ) *
-      ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho - 1 ) *
-      ( data$x1^(-rho) - data$x2^(-rho) ) ) )
-   result[ "rho" ] <- sum( -2 * resid * ( gamma *
-      log( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) ) *
-      ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho ) *
-      ( phi / rho^2 ) +
-      gamma * ( phi / rho ) *
-      ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho - 1 ) *
-      ( delta * log( data$x1 ) * data$x1^(-rho) +
-         ( 1 - delta ) * log( data$x2 ) * data$x2^(-rho) ) ) )
-   if( "phi" %in% names( par ) ) {
-      result[ "phi" ] <- sum( -2 * resid * ( gamma *
-         log( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) ) *
-         ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho ) *
-         ( -1 / rho ) ) )
+
+   derivCoef <- cesDerivCoef( par = par, data = data )
+
+   for( coefName in names( par ) ) {
+      result[ coefName ] <- sum( - 2 * resid * derivCoef[ , coefName ] )
    }
 
    return( result )
