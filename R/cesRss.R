3dff3e6e9a589313c90a8911969136323f9b9cb7
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-12 16:17:58 +0000

    cesRss() now uses cesCalc() to calculate the fitted values now
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1301 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesRss.R
+++ R/cesRss.R
@@ -1,13 +1,4 @@
 cesRss <- function( par, data ) {
-   gamma <- par[ "gamma" ]
-   delta <- par[ "delta" ]
-   rho <- par[ "rho" ]
-   if( "phi" %in% names( par ) ) {
-      phi <- par[ "phi" ]
-   } else {
-      phi <- 1
-   }
-   yHat <- gamma *
-      ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho )
+   yHat <- cesCalc( xNames = names( data )[ -1 ], data = data, coef = par )
    return( sum( ( data$y - yHat )^2 ) )
 }
