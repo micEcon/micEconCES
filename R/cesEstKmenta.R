a52f0afae4189a71dc680d8849df434c9ef7d79b
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-12 12:39:28 +0000

    renamed the element returned by cesEst() and cesEstKmenta "translog" (the estimated Kmenta approximation) as "kmenta"; improved the description of this element in the documentation of cesEst()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1294 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesEstKmenta.R
+++ R/cesEstKmenta.R
@@ -10,8 +10,8 @@ cesEstKmenta <- function( yName, xNames, data ){
       c( "b_1_2 = -b_1_1", "b_1_2 = -b_2_2" ))
 
    ## Estimating restricted model
-   result$translog <- systemfit( formula = formula(result$translog$est),
-      data = model.frame( result$translog$est ),
+   result$kmenta <- systemfit( formula = formula(result$translogUnr$est),
+      data = model.frame( result$translogUnr$est ),
       restrict.matrix = c( "eq1_b_1_2 = -eq1_b_1_1", "eq1_b_1_2 = -eq1_b_2_2" ))
 
    ## Parameter vector
@@ -19,45 +19,45 @@ cesEstKmenta <- function( yName, xNames, data ){
    names( result$coefficients ) <- c( "gamma", "delta", "rho", "phi" )
 
    ## Defining gamma
-   result$coefficients[ "gamma" ] <- exp( coef( result$translog )[ "eq1_(Intercept)" ] )
+   result$coefficients[ "gamma" ] <- exp( coef( result$kmenta )[ "eq1_(Intercept)" ] )
 
    ## Defining phi
-   result$coefficients[ "phi" ] <- coef( result$translog )[ "eq1_a_1" ] +
-                             coef( result$translog )[ "eq1_a_2" ]
+   result$coefficients[ "phi" ] <- coef( result$kmenta )[ "eq1_a_1" ] +
+                             coef( result$kmenta )[ "eq1_a_2" ]
 
    ## Defining delta
-   result$coefficients[ "delta" ] <- coef( result$translog )[ "eq1_a_1" ] /
+   result$coefficients[ "delta" ] <- coef( result$kmenta )[ "eq1_a_1" ] /
                                result$coefficients[ "phi" ]
 
    ## Defining rho
-   result$coefficients[ "rho" ] <- coef( result$translog )[ "eq1_b_1_2" ] /
-      ( coef( result$translog )[ "eq1_a_1" ] * coef( result$translog )[ "eq1_a_2" ] /
+   result$coefficients[ "rho" ] <- coef( result$kmenta )[ "eq1_b_1_2" ] /
+      ( coef( result$kmenta )[ "eq1_a_1" ] * coef( result$kmenta )[ "eq1_a_2" ] /
          result$coefficients[ "phi" ] )
 
    ## Delta method
    jacobian <- matrix( 0, nrow = length( result$coefficients ),
-      ncol = length( coef( result$translog )))
+      ncol = length( coef( result$kmenta )))
    rownames( jacobian ) <- names( result$coefficients )
-   colnames( jacobian ) <- names( coef( result$translog ))
+   colnames( jacobian ) <- names( coef( result$kmenta ))
 
    jacobian[ "gamma", "eq1_(Intercept)" ] <-
-      exp( coef( result$translog )[ "eq1_(Intercept)" ] )
+      exp( coef( result$kmenta )[ "eq1_(Intercept)" ] )
    jacobian[ "phi", c( "eq1_a_1", "eq1_a_2" )] <- 1
    jacobian[ "delta", "eq1_a_1" ] <- 1 / result$coefficients[ "phi" ] -
-      coef( result$translog )[ "eq1_a_1" ] / result$coefficients[ "phi" ]^2
+      coef( result$kmenta )[ "eq1_a_1" ] / result$coefficients[ "phi" ]^2
    jacobian[ "delta", "eq1_a_2" ] <-
-      - coef( result$translog )[ "eq1_a_1" ] / result$coefficients[ "phi" ]^2
-   jacobian[ "rho", "eq1_a_1" ] <- coef( result$translog )[ "eq1_b_1_2" ] /
-      coef( result$translog )[ "eq1_a_1" ] * coef( result$translog )[ "eq1_a_2" ] -
-      coef( result$translog )[ "eq1_b_1_2" ] * result$coefficients[ "phi" ] /
-      coef( result$translog )[ "eq1_a_1" ]^2 / coef( result$translog )[ "eq1_a_2" ]^3
-   jacobian[ "rho", "eq1_a_2" ] <- coef( result$translog )[ "eq1_b_1_2" ] /
-      coef( result$translog )[ "eq1_a_1" ] * coef( result$translog )[ "eq1_a_2" ] -
-      coef( result$translog )[ "eq1_b_1_2" ] * result$coefficients[ "phi" ] /
-      coef( result$translog )[ "eq1_a_1" ]^3 / coef( result$translog )[ "eq1_a_2" ]^2
+      - coef( result$kmenta )[ "eq1_a_1" ] / result$coefficients[ "phi" ]^2
+   jacobian[ "rho", "eq1_a_1" ] <- coef( result$kmenta )[ "eq1_b_1_2" ] /
+      coef( result$kmenta )[ "eq1_a_1" ] * coef( result$kmenta )[ "eq1_a_2" ] -
+      coef( result$kmenta )[ "eq1_b_1_2" ] * result$coefficients[ "phi" ] /
+      coef( result$kmenta )[ "eq1_a_1" ]^2 / coef( result$kmenta )[ "eq1_a_2" ]^3
+   jacobian[ "rho", "eq1_a_2" ] <- coef( result$kmenta )[ "eq1_b_1_2" ] /
+      coef( result$kmenta )[ "eq1_a_1" ] * coef( result$kmenta )[ "eq1_a_2" ] -
+      coef( result$kmenta )[ "eq1_b_1_2" ] * result$coefficients[ "phi" ] /
+      coef( result$kmenta )[ "eq1_a_1" ]^3 / coef( result$kmenta )[ "eq1_a_2" ]^2
    jacobian[ "rho", "eq1_b_1_2" ] <-  result$coefficients[ "phi" ] /
-      coef( result$translog )[ "eq1_a_1" ] / coef( result$translog )[ "eq1_a_2" ]
-   result$vcov <- jacobian %*% vcov( result$translog ) %*%  t( jacobian )
+      coef( result$kmenta )[ "eq1_a_1" ] / coef( result$kmenta )[ "eq1_a_2" ]
+   result$vcov <- jacobian %*% vcov( result$kmenta ) %*%  t( jacobian )
 
    return(result)
 }
