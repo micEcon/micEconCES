9f43bd67e82d6e169e7271ed2af416471dfa1c7d
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-14 10:08:48 +0000

    fixed bug in cesRssDeriv() that prevented nlm() from using analytical derivatives
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1316 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesRssDeriv.R
+++ R/cesRssDeriv.R
@@ -1,14 +1,17 @@
 cesRssDeriv <- function( par, data ) {
-   result <- par
 
+   # calculate fitted values and residuals
    yHat <- cesCalc( xNames = names( data )[ -1 ], data = data, coef = par )
    resid <- data$y - yHat
 
+   # obtain derivatives of the CES with respect to coefficients
    derivCoef <- cesDerivCoef( par = par, data = data )
 
+   # prepare vector of gradients (to be returned)
+   result <- numeric( length( par ) )
+   names( result ) <- colnames( derivCoef )
    for( coefName in colnames( derivCoef ) ) {
       result[ coefName ] <- sum( - 2 * resid * derivCoef[ , coefName ] )
    }
-
    return( result )
 }
