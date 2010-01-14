b7587ed7a716d1d300ebf7326c0b1fb267045ec9
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-14 12:51:50 +0000

    if some fitted values cannot be calculated because some coefficients have bad values, cesRss() no longer returns NA but Inf
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1320 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesRss.R
+++ R/cesRss.R
@@ -1,4 +1,10 @@
 cesRss <- function( par, data ) {
+
    yHat <- cesCalc( xNames = names( data )[ -1 ], data = data, coef = par )
-   return( sum( ( data$y - yHat )^2 ) )
+
+   result <- sum( ( data$y - yHat )^2 )
+   if( is.na( result ) ) {
+      result <- Inf
+   }
+   return( result )
 }
