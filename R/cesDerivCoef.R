38d5812702414f4dbcc2505538c01ad725615d99
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-17 13:26:59 +0000

    cesDerivCoef() now determines whether the model has constant or variable returns to scale by comparing the number of exogenous variables with the number of coefficients; the result (CRS or VRS) is used in this function now
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1341 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesDerivCoef.R
+++ R/cesDerivCoef.R
@@ -1,14 +1,17 @@
 cesDerivCoef <- function( par, data ) {
 
+   # check for VRS
+   vrs <- length( par ) >= 4
+
    # derivatives of the CES with respect to the coefficients/parameters
    result <- matrix( NA, nrow = nrow( data ), ncol = length( par ) )
-   colnames( result ) <- c( "gamma", "delta", "rho", "phi" )[ 1:length( par ) ]
-   names( par ) <- c( "gamma", "delta", "rho", "phi" )[ 1:length( par ) ]
+   colnames( result ) <- c( "gamma", "delta", "rho", "phi" )[ 1:( 3 + vrs ) ]
+   names( par ) <- c( "gamma", "delta", "rho", "phi" )[ 1:( 3 + vrs ) ]
 
    gamma <- par[ "gamma" ]
    delta <- par[ "delta" ]
    rho <- par[ "rho" ]
-   if( "phi" %in% names( par ) ) {
+   if( vrs ) {
       phi <- par[ "phi" ]
    } else {
       phi <- 1
@@ -34,7 +37,7 @@ cesDerivCoef <- function( par, data ) {
          ( 1 - delta ) * log( data$x2 ) * data$x2^(-rho) )
 
    # derivatives with respect to phi
-   if( "phi" %in% names( par ) ) {
+   if( vrs ) {
       result[ , "phi" ] <- gamma *
          log( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) ) *
          ( delta * data$x1^(-rho) + ( 1 - delta ) * data$x2^(-rho) )^( -phi / rho ) *
