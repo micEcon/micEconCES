0c69cfdc17c0dafac56b14ec4a7ea64c28824991
Arne Henningsen <arne.henningsen@gmail.com>
2010-01-18 13:50:19 +0000

    added test script and corresponding output file to test aidsCalc() when rho is around 0
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1352 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ tests/rhoZero.R
@@ -0,0 +1,37 @@
+# load the micEconCES package
+library( micEconCES )
+
+# seed for random number generation
+set.seed( 123 )
+
+# number of observations
+nObs <- 20
+
+# create data set with explanatory variables
+cesData <- data.frame( xx1 = rchisq( nObs, 10 ), xx2 = rchisq( nObs, 10 ) )
+
+# names of explanatory variables
+xxNames <- c( "xx1", "xx2" )
+
+# vector with values around 0 for coefficient "rho"
+rhos = c( -exp(-(1:20)),0,exp(-(20:1)) )
+
+# matrix for returned endogenous variables
+y <- matrix( NA, nrow = length( rhos ), ncol = nObs )
+rownames( y ) <- c( -(1:20), 0, (20:1) )
+
+# calculate endogenous variables
+for( i in 1:length( rhos ) ) {
+   # coefficients
+   cesCoef <- c( gamma = 1, delta = 0.6, rho = rhos[ i ], nu = 1.1 )
+   y[ i, ] <- cesCalc( xNames = xxNames, data = cesData, coef = cesCoef )
+}
+
+# print matrix of endogenous variables
+print( y )
+
+# print endogenous variables for different rhos (mean-adjusted)
+for( i in 1:nObs ) {
+   print( y[ , i, drop = FALSE ] - mean( y[-21,i] ) )
+}
+
