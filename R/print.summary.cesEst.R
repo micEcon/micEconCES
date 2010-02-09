9afe312e0eed93f2a5e33ec770ee0bd5a27c9d39
Arne Henningsen <arne.henningsen@gmail.com>
2010-02-09 10:13:31 +0000

    if the model was estimated by a grid search for rho using cesEstGridRho(), print.summary.cesEst() now writes "one-dimensional grid search for coefficient 'rho'" instead of "Coefficient 'rho' was fixed at ..."
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1369 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/print.summary.cesEst.R
+++ R/print.summary.cesEst.R
@@ -19,7 +19,9 @@ print.summary.cesEst <- function( x, digits = max( 3, getOption( "digits" ) - 3 
    } else {
       cat( "non-linear least-squares using the '", x$method, "' optimizer\n",
          sep = "" )
-      if( !is.null( x$rho ) ) {
+      if( "cesEstRhoGrid" %in% class( x ) ) {
+         cat( "and a one-dimensional grid search for coefficient 'rho'\n" )
+      } else if( !is.null( x$rho ) ) {
          cat( "Coefficient 'rho' was fixed at", x$rho, "\n" )
       }
       if( ! x$method %in% c( "SANN", "DE" ) ) {
