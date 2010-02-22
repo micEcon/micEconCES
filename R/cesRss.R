a81804bea73a7200b8d70edeb217c2b2f698bee9
Arne Henningsen <arne.henningsen@gmail.com>
2010-02-22 09:58:13 +0000

    the internal function cesRss() now has an argument "rhoApprox" (defaults to 5e-6) that is passed to cesCalc()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1387 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesRss.R
+++ R/cesRss.R
@@ -1,9 +1,11 @@
-cesRss <- function( par, yName, xNames, data, vrs, rho = NULL ) {
+cesRss <- function( par, yName, xNames, data, vrs, rho = NULL,
+      rhoApprox = 5e-6 ) {
 
    # add coefficient 'rho' if it is fixed
    par <- cesCoefAddRho( coef = par, vrs = vrs, rho = rho )
 
-   yHat <- cesCalc( xNames = xNames, data = data, coef = par )
+   yHat <- cesCalc( xNames = xNames, data = data, coef = par,
+      rhoApprox = rhoApprox )
 
    result <- sum( ( data[[ yName ]] - yHat )^2 )
    if( is.na( result ) ) {
