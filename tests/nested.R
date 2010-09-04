b7c66c7d0620b986bb9e4ea22a27f781759d51f9
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-09-04 12:50:00 +0000

    added test for cesCalc() with nested CES with 4 inputs and VRS
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1505 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- tests/nested.R
+++ tests/nested.R
@@ -4,10 +4,19 @@ data( "MishraCES" )
 
 b <- c( "gamma" = 200, "delta_1" = 0.6, "delta_2" = 0.3, "rho_1" = 0.5,
    "rho_2" = -0.17, "rho" = 0.6 )
+bVrs <- c( b, "nu" = 1.05 )
+xNames <- c( "X1", "X2", "X3", "X4" )
 
-MishraCES$Y2 <- cesCalc( xNames = c( "X1", "X2", "X3", "X4" ), 
+
+## checking cesCalc()
+MishraCES$Y2 <- cesCalc( xNames = xNames, 
    data = MishraCES, coef = b, nested = TRUE )
 
 MishraCES$Y2
 
 all.equal( MishraCES$Y, MishraCES$Y2 )
+
+# VRS
+MishraCES$yVrs <- cesCalc( xNames = xNames, 
+   data = MishraCES, coef = bVrs, nested = TRUE )
+MishraCES$yVrs
