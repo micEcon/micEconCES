7c1fafa06467165702fa689e2237856e641c8448
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-09-11 07:49:13 +0000

    simplified (shortened) code in cesCoefBounds()
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1526 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesCoefBounds.R
+++ R/cesCoefBounds.R
@@ -1,6 +1,6 @@
 cesCoefBounds <- function( vrs, returnRho, method, lower, nested = FALSE ) {
 
-   if( method %in% c( "L-BFGS-B", "PORT" ) ) {
+   if( method %in% c( "L-BFGS-B", "PORT", "DE" ) ) {
       if( lower ) {
          if( nested ) {
             result <- c( 0, 0, 0, -1, -1 )
@@ -20,28 +20,14 @@ cesCoefBounds <- function( vrs, returnRho, method, lower, nested = FALSE ) {
       if( vrs ) {
          result <- c( result, ifelse( lower, 0, Inf ) )
       }
-   } else if( method == "DE" ) {
-      if( lower ) {
-         if( nested ) {
-            result <- c( 0, 0, 0, -1, -1 )
-         } else {
-            result <- c( 0, 0 )
-         }
-      } else {
-         if( nested ) {
-            result <- c( 1e10, 1, 1, 10, 10 )
-         } else {
-            result <- c( 1e10, 1 )
-         }
-      }
-      if( returnRho ) {
-         result <- c( result, ifelse( lower, -1, 10 ) )
-      }
-      if( vrs ) {
-         result <- c( result, ifelse( lower, 0, 10 ) )
-      }
    } else {
       result <- ifelse( lower, -Inf, Inf )
    }
+
+   if( method == "DE" ) {
+      result[ 1:2 ][ !is.finite( result[ 1:2 ] ) ] <- 1e10
+      result[ !is.finite( result ) ] <- 10
+   }
+
    return( result )
 }
