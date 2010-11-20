1be96da80d090a9092ef8d4e406babc7ce2a946f
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-20 17:36:31 +0000

    now cesCalcN3() and hence, cesCalc() can calculate the dependent variable of the 3-input nested CES even if both rho_1=0 and rho=0
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1587 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesCalcN3.R
+++ R/cesCalcN3.R
@@ -1,13 +1,22 @@
 cesCalcN3 <- function( xNames, data, coef ) {
 
    if( coef[ "rho" ] == 0 ) {
-      result <- coef[ "gamma_2" ] *
-         exp( coef[ "nu" ] * ( coef[ "delta_2" ] *
-            ( log( coef[ "gamma_1" ] ) -
-               log( coef[ "delta_1" ] * data[[ xNames[ 1 ] ]]^( -coef[ "rho_1" ] ) +
-                  ( 1 - coef[ "delta_1" ] ) * data[[ xNames[ 2 ] ]]^( -coef[ "rho_1" ] ) ) /
-               coef[ "rho_1" ] ) +
-            ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 3 ] ]] ) ) )
+      if( coef[ "rho_1" ] == 0 ) {
+         result <- coef[ "gamma_2" ] *
+            exp( coef[ "nu" ] * 
+               ( coef[ "delta_2" ] * ( log( coef[ "gamma_1" ] ) +
+                  coef[ "delta_1" ] * log( data[[ xNames[ 1 ] ]] ) +
+                  ( 1 - coef[ "delta_1" ] ) * log( data[[ xNames[ 2 ] ]] ) ) +
+               ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 3 ] ]] ) ) )
+      } else {
+         result <- coef[ "gamma_2" ] *
+            exp( coef[ "nu" ] * ( coef[ "delta_2" ] *
+               ( log( coef[ "gamma_1" ] ) -
+                  log( coef[ "delta_1" ] * data[[ xNames[ 1 ] ]]^( -coef[ "rho_1" ] ) +
+                     ( 1 - coef[ "delta_1" ] ) * data[[ xNames[ 2 ] ]]^( -coef[ "rho_1" ] ) ) /
+                  coef[ "rho_1" ] ) +
+               ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 3 ] ]] ) ) )
+      }
    } else if( coef[ "rho_1" ] == 0 ) {
       result <- coef[ "gamma_2" ] * 
          ( coef[ "delta_2" ] * coef[ "gamma_1" ]^( -coef[ "rho" ] ) *
