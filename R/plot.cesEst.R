d8479b2a0e6fef0642886ac4a4fc75e5199d125a
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-21 15:22:47 +0000

    updated the plot() method for objects of class cesEst so that it produces correct graphs also if the grid search was done with different values of rho_1
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1606 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/plot.cesEst.R
+++ R/plot.cesEst.R
@@ -2,16 +2,25 @@ plot.cesEst <- function( x, ... ) {
 
    if( is.null( x$allRhoSum ) ) {
       stop( "the 'plot' method for objects of class 'cesEst' can be applied",
-         " only if the CES function was estimated by grid search for 'rho',",
-         " i.e. 'cesEst' was called with argument 'rho' set to a vector",
+         " only if the CES function was estimated by grid search for 'rho_1'",
+         " or 'rho',",
+         " i.e. 'cesEst' was called with argument 'rho1' or 'rho' set to a vector",
          " with more than one element" )
    }
 
-   rhoVal <- x$allRhoSum$rho
+   if( !is.null( x$allRhoSum[[ "rho1" ]] ) ) {
+      rhoVal <- x$allRhoSum[[ "rho1" ]]
+      rhoName <- "rho_1"
+   } else if( !is.null( x$allRhoSum[[ "rho" ]] ) ) {
+      rhoVal <- x$allRhoSum[[ "rho" ]]
+      rhoName <- "rho"
+   } else {
+      stop( "either 'x$allRhoSum$rho1' or 'x$allRhoSum$rho' must be non-NULL" )
+   }
    rssVal <- x$allRhoSum$rss
 
    plot.default( x = rhoVal, y = rssVal, type = "o", pch = 19,
-      xlab = "rho", ylab = "rss", ... )
+      xlab = rhoName, ylab = "rss", ... )
 
    # mark estimations that did not converge
    nonConv <- !is.na( x$allRhoSum$convergence )& !x$allRhoSum$convergence
