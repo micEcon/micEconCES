6a85547b8214636b7c6d3e6b2aca5329ac1c273c
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-21 22:15:45 +0000

    replaced internal variables (vectors) coef00, coef01, coef10, and coef11 by an 3-dimensional array (this should have no user-visible effect
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1615 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesInterN3.R
+++ R/cesInterN3.R
@@ -4,49 +4,48 @@ cesInterN3 <- function( funcName, par, xNames, data, rhoApprox ) {
       rho1 <- par[ "rho_1" ]
       rho <- par[ "rho" ]
 
-      coef00 <- par
-      coef01 <- par
-      coef10 <- par
-      coef11 <- par
+      coefArray <- array( par, c( length( par ), 2, 2 ) )
+      dimnames( coefArray ) <- list( names( par ), 
+         c( "rho = 0", "rho = E" ),  c( "rho_1 = 0", "rho_1 = E" ) )
       wa <- 1
       wb <- 1
       if( rho == 0 ) {
          wa <- 0
       } else if( abs( rho ) <= rhoApprox ) {
-         coef00[ "rho" ] <- coef01[ "rho" ] <- 0
-         coef10[ "rho" ] <- coef11[ "rho" ] <- 
+         coefArray[ "rho", 1, 1 ] <- coefArray[ "rho", 1, 2 ] <- 0
+         coefArray[ "rho", 2, 1 ] <- coefArray[ "rho", 2, 2 ] <- 
             rhoApprox * (-1)^( rho < 0 )
          wa <- abs( rho ) / rhoApprox
       }
       if( rho1 == 0 ) {
          wb <- 0
       } else if( abs( rho1 ) <= rhoApprox ) {
-         coef00[ "rho_1" ] <- coef10[ "rho_1" ] <- 0
-         coef01[ "rho_1" ] <- coef11[ "rho_1" ] <- 
+         coefArray[ "rho_1", 1, 1 ] <- coefArray[ "rho_1", 2, 1 ] <- 0
+         coefArray[ "rho_1", 1, 2 ] <- coefArray[ "rho_1", 2, 2 ] <- 
             rhoApprox * (-1)^( rho1 < 0 )
          wb <- abs( rho1 ) / rhoApprox
       }
       if( wa != 1 && wb != 1 ) {
          result00 <- do.call( funcName, 
-            args = list( coef = coef00, data = data, xNames = xNames ) )
+            args = list( coef = coefArray[ , 1, 1 ], data = data, xNames = xNames ) )
       } else {
          result00 <- 0
       }
       if( wa != 1 && wb != 0 ) {
          result01 <- do.call( funcName, 
-            args = list( coef = coef01, data = data, xNames = xNames ) )
+            args = list( coef = coefArray[ , 1, 2 ], data = data, xNames = xNames ) )
       } else {
          result01 <- 0
       }
       if( wa != 0 && wb != 1 ) {
          result10 <- do.call( funcName, 
-            args = list( coef = coef10, data = data, xNames = xNames ) )
+            args = list( coef = coefArray[ , 2, 1 ], data = data, xNames = xNames ) )
       } else {
          result10 <- 0
       }
       if( wa != 0 && wb != 0 ) {
          result11 <- do.call( funcName, 
-            args = list( coef = coef11, data = data, xNames = xNames ) )
+            args = list( coef = coefArray[ , 2, 2 ], data = data, xNames = xNames ) )
       } else {
          result11 <- 0
       }
