8a18ce70a848c6e8be94045ea6228db753bf2fe1
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-21 00:32:22 +0000

    the internal function cesCoefNames() has two additional arguments, returnRho1 and returnRho2, now
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1595 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesCoefNames.R
+++ R/cesCoefNames.R
@@ -1,15 +1,22 @@
-cesCoefNames <- function( nExog, vrs, returnRho = TRUE, nested = FALSE ) {
+cesCoefNames <- function( nExog, vrs, returnRho1 = TRUE, returnRho2 = TRUE, 
+      returnRho = TRUE, nested = FALSE ) {
 
    if( nExog == 2 ) {
       result <- c( "gamma", "delta" )
    } else if( !nested ) {
       result <- c( "gamma", paste( "delta", 1:nExog, sep = "_" ) )
    } else if( nested && nExog == 3 ) {
-      result <- c( "gamma_1", "gamma_2", "delta_1", "delta_2", "rho_1" )
+      result <- c( "gamma_1", "gamma_2", "delta_1", "delta_2" )
    } else if( nested && nExog == 4 ) {
-      result <- c( "gamma", "delta_1", "delta_2", "delta_3", "rho_1", "rho_2" )
+      result <- c( "gamma", "delta_1", "delta_2", "delta_3" )
    } else {
       stop( "internal error: non-supported arguments to cesCoefNames()" )
+   }
+   if( returnRho1 && nested ) {
+      result <- c( result, "rho_1" )
+   }
+   if( returnRho2 && nested && nExog == 4 ) {
+      result <- c( result, "rho_2" )
    }
    if( returnRho ) {
       result <- c( result, "rho" )
