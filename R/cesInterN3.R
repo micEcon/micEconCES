9ce6d98c1d9e61abb477a074588dc135ba925f8d
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-21 22:59:37 +0000

    further simplified the code for the interpolation in cesInterN3(), no by replacing scalar variables wa and wb by the vector weights (these modifications should not be user-visible)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1618 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesInterN3.R
+++ R/cesInterN3.R
@@ -7,42 +7,44 @@ cesInterN3 <- function( funcName, par, xNames, data, rhoApprox ) {
       coefArray <- array( par, c( length( par ), 2, 2 ) )
       dimnames( coefArray ) <- list( names( par ), 
          c( "rho = 0", "rho = E" ),  c( "rho_1 = 0", "rho_1 = E" ) )
-      wa <- 1
-      wb <- 1
+      weights <- c( 1, 1 )
+      names( weights ) <- c( "rho = E", "rho_1 = E" )
       if( rho == 0 ) {
-         wa <- 0
+         weights[ "rho = E" ] <- 0
       } else if( abs( rho ) <= rhoApprox ) {
          coefArray[ "rho", "rho = 0", ] <- 0
          coefArray[ "rho", "rho = E", ] <- rhoApprox * (-1)^( rho < 0 )
-         wa <- abs( rho ) / rhoApprox
+         weights[ "rho = E" ] <- abs( rho ) / rhoApprox
       }
       if( rho1 == 0 ) {
-         wb <- 0
+         weights[ "rho_1 = E" ] <- 0
       } else if( abs( rho1 ) <= rhoApprox ) {
          coefArray[ "rho_1", , "rho_1 = 0" ] <- 0
          coefArray[ "rho_1", , "rho_1 = E" ] <- rhoApprox * (-1)^( rho1 < 0 )
-         wb <- abs( rho1 ) / rhoApprox
+         weights[ "rho_1 = E" ] <- abs( rho1 ) / rhoApprox
       }
       result <- array( 0, c( nrow( data ), 2, 2 ) )
-      if( wa != 1 && wb != 1 ) {
+      if( weights[ 1 ] != 1 && weights[ 2 ] != 1 ) {
          result[ , 1, 1 ] <- do.call( funcName, 
             args = list( coef = coefArray[ , 1, 1 ], data = data, xNames = xNames ) )
       }
-      if( wa != 1 && wb != 0 ) {
+      if( weights[ 1 ] != 1 && weights[ 2 ] != 0 ) {
          result[ , 1, 2 ] <- do.call( funcName, 
             args = list( coef = coefArray[ , 1, 2 ], data = data, xNames = xNames ) )
       }
-      if( wa != 0 && wb != 1 ) {
+      if( weights[ 1 ] != 0 && weights[ 2 ] != 1 ) {
          result[ , 2, 1 ] <- do.call( funcName, 
             args = list( coef = coefArray[ , 2, 1 ], data = data, xNames = xNames ) )
       }
-      if( wa != 0 && wb != 0 ) {
+      if( weights[ 1 ] != 0 && weights[ 2 ] != 0 ) {
          result[ , 2, 2 ] <- do.call( funcName, 
             args = list( coef = coefArray[ , 2, 2 ], data = data, xNames = xNames ) )
       }
       result <- 
-         wa * wb * result[ , 2, 2 ] + ( 1 - wa ) * wb * result[ , 1, 2 ] +
-         wa * ( 1 - wb ) * result[ , 2, 1 ] + ( 1- wa ) * ( 1 - wb ) * result[ , 1, 1 ]
+         weights[ 1 ] * weights[ 2 ] * result[ , 2, 2 ] + 
+         ( 1 - weights[ 1 ] ) * weights[ 2 ] * result[ , 1, 2 ] +
+         weights[ 1 ] * ( 1 - weights[ 2 ] ) * result[ , 2, 1 ] + 
+         ( 1- weights[ 1 ] ) * ( 1 - weights[ 2 ] ) * result[ , 1, 1 ]
 
    return( result )
 }
