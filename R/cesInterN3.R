6d8d3c9483f70b9d63fb059ad7adcfd2035db1f3
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-22 06:17:52 +0000

    further modified the code for the interpolation in cesInterN3(), now the specification of the weights is more consistent (these modifications should not be user-visible)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1622 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesInterN3.R
+++ R/cesInterN3.R
@@ -7,24 +7,24 @@ cesInterN3 <- function( funcName, par, xNames, data, rhoApprox ) {
       coefArray <- array( par, c( length( par ), 2, 2 ) )
       dimnames( coefArray ) <- list( names( par ), 
          c( "rho = 0", "rho = E" ),  c( "rho_1 = 0", "rho_1 = E" ) )
-      weights <- c( 1, 1 )
-      names( weights ) <- c( "rho = E", "rho_1 = E" )
+      weights <- c( 0, 0 )
+      names( weights ) <- c( "rho = 0", "rho_1 = 0" )
       if( rho == 0 ) {
-         weights[ "rho = E" ] <- 0
+         weights[ "rho = 0" ] <- 1
       } else if( abs( rho ) <= rhoApprox ) {
          coefArray[ "rho", "rho = 0", ] <- 0
          coefArray[ "rho", "rho = E", ] <- rhoApprox * (-1)^( rho < 0 )
-         weights[ "rho = E" ] <- abs( rho ) / rhoApprox
+         weights[ "rho = 0" ] <- 1 - abs( rho ) / rhoApprox
       }
       if( rho1 == 0 ) {
-         weights[ "rho_1 = E" ] <- 0
+         weights[ "rho_1 = 0" ] <- 1
       } else if( abs( rho1 ) <= rhoApprox ) {
          coefArray[ "rho_1", , "rho_1 = 0" ] <- 0
          coefArray[ "rho_1", , "rho_1 = E" ] <- rhoApprox * (-1)^( rho1 < 0 )
-         weights[ "rho_1 = E" ] <- abs( rho1 ) / rhoApprox
+         weights[ "rho_1 = 0" ] <- 1 - abs( rho1 ) / rhoApprox
       }
       result <- array( 0, c( nrow( data ), 2, 2 ) )
-      weightMatrix <- cbind( 1 - weights, weights )
+      weightMatrix <- cbind( weights, 1 - weights )
       for( i in 1:2 ) {
          for( j in 1:2 ) {
             if( weightMatrix[ 1, i ] != 0 && weightMatrix[ 2, j ] != 0 ) {
