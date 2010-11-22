25fddc5112147bf39c0ffd58aaa69b149cd7ed28
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-11-22 07:37:49 +0000

    further modified the code for the interpolation in cesInterN3(): changed preparation of coefficient array and weights so that it will be easier in the future to use a loop for this (these modifications should not be user-visible)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1625 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesInterN3.R
+++ R/cesInterN3.R
@@ -1,23 +1,22 @@
 cesInterN3 <- function( funcName, par, xNames, data, rhoApprox ) {
 
       # interpolation if rho and/or rho_1 are close to zero
-      rho1 <- par[ "rho_1" ]
-      rho <- par[ "rho" ]
-
       coefArray <- array( par, c( length( par ), 2, 2 ) )
       dimnames( coefArray ) <- list( names( par ), 
          c( "rho = 0", "rho = E" ),  c( "rho_1 = 0", "rho_1 = E" ) )
       weights <- c( 0, 0 )
       names( weights ) <- c( "rho = 0", "rho_1 = 0" )
-      if( abs( rho ) <= rhoApprox ) {
-         coefArray[ "rho", "rho = 0", ] <- 0
-         coefArray[ "rho", "rho = E", ] <- rhoApprox * (-1)^( rho < 0 )
-         weights[ "rho = 0" ] <- 1 - abs( rho ) / rhoApprox
+      if( abs( par[ "rho" ] ) <= rhoApprox ) {
+         coefArray[ "rho", 1, ] <- 0
+         coefArray[ "rho", 2, ] <- 
+            rhoApprox * (-1)^( par[ "rho" ] < 0 )
+         weights[ "rho = 0" ] <- 1 - abs( par[ "rho" ] ) / rhoApprox
       }
-      if( abs( rho1 ) <= rhoApprox ) {
-         coefArray[ "rho_1", , "rho_1 = 0" ] <- 0
-         coefArray[ "rho_1", , "rho_1 = E" ] <- rhoApprox * (-1)^( rho1 < 0 )
-         weights[ "rho_1 = 0" ] <- 1 - abs( rho1 ) / rhoApprox
+      if( abs( par[ "rho_1" ] ) <= rhoApprox ) {
+         coefArray[ "rho_1", , 1 ] <- 0
+         coefArray[ "rho_1", , 2 ] <- 
+            rhoApprox * (-1)^( par[ "rho_1" ] < 0 )
+         weights[ "rho_1 = 0" ] <- 1 - abs( par[ "rho_1" ] ) / rhoApprox
       }
       result <- 0
       weightMatrix <- cbind( weights, 1 - weights )
