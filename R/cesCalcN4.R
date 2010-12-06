4f9b3465c3236a073604d0f5462e980e3d6e5629
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-12-06 23:09:49 +0000

    if both rho_1 = 0 and rho_2 = 0, the dependent variable of the 4-input nested CES is calculated by taking the double limit for rho_1 -> 0 and rho_2 -> 0 now
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1643 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesCalcN4.R
+++ R/cesCalcN4.R
@@ -10,14 +10,27 @@ cesCalcN4 <- function( xNames, data, coef ) {
             ( coef[ "delta_3" ] * log( B1 ) / coef[ "rho_1" ] +
                ( 1 - coef[ "delta_3" ] ) * log( B2 ) / coef[ "rho_2" ] ) )
    } else if( coef[ "rho_1" ] == 0 ) {
-      result <- coef[ "gamma" ] *
-         ( coef[ "delta_3" ] * 
-            exp( coef[ "rho" ] * 
-               ( - coef[ "delta_1" ] * log( data[[ xNames[ 1 ] ]] ) -
-               ( 1 - coef[ "delta_1" ] ) * log( data[[ xNames[ 2 ] ]] ) ) ) +
-            ( 1 - coef[ "delta_3" ] ) * 
-               B2^( coef[ "rho" ] / coef[ "rho_2" ] ) 
-         )^( - ( coef[ "nu" ] / coef[ "rho" ] ) )
+      if( coef[ "rho_2" ] == 0 ) {
+         result <- coef[ "gamma" ] *
+            ( coef[ "delta_3" ] * 
+               exp( coef[ "rho" ] * 
+                  ( - coef[ "delta_1" ] * log( data[[ xNames[ 1 ] ]] ) -
+                  ( 1 - coef[ "delta_1" ] ) * log( data[[ xNames[ 2 ] ]] ) ) ) +
+               ( 1 - coef[ "delta_3" ] ) * 
+               exp( coef[ "rho" ] * 
+                  ( - coef[ "delta_2" ] * log( data[[ xNames[ 3 ] ]] ) -
+                     ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 4 ] ]] ) ) ) 
+            )^( - ( coef[ "nu" ] / coef[ "rho" ] ) )
+      } else {
+         result <- coef[ "gamma" ] *
+            ( coef[ "delta_3" ] * 
+               exp( coef[ "rho" ] * 
+                  ( - coef[ "delta_1" ] * log( data[[ xNames[ 1 ] ]] ) -
+                  ( 1 - coef[ "delta_1" ] ) * log( data[[ xNames[ 2 ] ]] ) ) ) +
+               ( 1 - coef[ "delta_3" ] ) * 
+                  B2^( coef[ "rho" ] / coef[ "rho_2" ] ) 
+            )^( - ( coef[ "nu" ] / coef[ "rho" ] ) )
+      }
    } else if( coef[ "rho_2" ] == 0 ) {
       result <- coef[ "gamma" ] *
          ( coef[ "delta_3" ] * B1^( coef[ "rho" ] / coef[ "rho_1" ] ) +
