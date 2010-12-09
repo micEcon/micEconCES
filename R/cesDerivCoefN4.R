5cba28e4b1cdc05e8922b5e606b4ac58904ced15
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-12-09 00:12:07 +0000

    if rho_1 equals zero, cesDerivCoefN4Rho2() now calculates the derivative of the 4-input nested CES function with respect to rho_2 by its limit for rho_1 -> zero
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1658 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesDerivCoefN4.R
+++ R/cesDerivCoefN4.R
@@ -269,6 +269,24 @@ cesDerivCoefN4Rho2 <- function( coef, data, xNames ) {
          exp( - coef[ "nu" ] *
             ( coef[ "delta_3" ] * log( B1 ) / coef[ "rho_1" ] +
                ( 1 - coef[ "delta_3" ] ) * log( B2 ) / coef[ "rho_2" ] ) )
+   } else if( coef[ "rho_1" ] == 0 ) {
+      result <- coef[ "gamma" ] * ( -coef[ "nu" ] / coef[ "rho" ] ) *
+         ( coef[ "delta_3" ] *
+            exp( coef[ "rho" ] *
+               ( - coef[ "delta_1" ] * log( data[[ xNames[ 1 ] ]] ) -
+                  ( 1 - coef[ "delta_1" ] ) * log( data[[ xNames[ 2 ] ]] ) ) ) +
+            ( 1 - coef[ "delta_3" ] ) * 
+               B2^( coef[ "rho" ] / coef[ "rho_2" ] ) 
+         )^( - coef[ "nu" ] / coef[ "rho" ] - 1 ) *
+         ( ( 1 - coef[ "delta_3" ] ) * log( B2 ) * 
+            B2^( coef[ "rho" ] / coef[ "rho_2" ] ) *
+            ( - coef[ "rho" ] / coef[ "rho_2" ]^2 ) +
+            ( 1 - coef[ "delta_3" ] ) * ( coef[ "rho" ] / coef[ "rho_2" ] ) *
+            B2^( coef[ "rho" ] / coef[ "rho_2" ] - 1 ) *
+            ( -coef[ "delta_2" ] * log( data[[ xNames[ 3 ] ]] ) * 
+               data[[ xNames[ 3 ] ]]^( - coef[ "rho_2" ]) - 
+               ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 4 ] ]] ) * 
+               data[[ xNames[ 4 ] ]]^( - coef[ "rho_2" ] ) ) )
    } else {
          result <- coef[ "gamma" ] * ( -coef[ "nu" ] / coef[ "rho" ] ) * 
             B^((-coef[ "nu" ]-coef[ "rho" ])/coef[ "rho" ]) *
