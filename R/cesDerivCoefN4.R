24ecf652c91779decbefa247e6b78e2d7bcd6883
Geraldine Henningsen <geraldine.henningsen@gmail.com>
2010-12-12 17:43:26 +0000

    if both rho_1 and rho_2 are equal to zero, cesDerivCoefN4Rho2() now calculates the derivative of the 4-input nested CES function with respect to rho_2 by its limit for rho_1 -> 0, rho_2 -> 0
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1696 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/cesDerivCoefN4.R
+++ R/cesDerivCoefN4.R
@@ -540,21 +540,33 @@ cesDerivCoefN4Rho2 <- function( coef, data, xNames ) {
                   ( 1 - coef[ "delta_3" ] ) * log( B2 ) / coef[ "rho_2" ] ) )
       }
    } else if( coef[ "rho_1" ] == 0 ) {
-      result <- coef[ "gamma" ] * ( -coef[ "nu" ] / coef[ "rho" ] ) *
-         ( coef[ "delta_3" ] *
-            exp( coef[ "rho" ] * ( - BB1 ) ) +
-            ( 1 - coef[ "delta_3" ] ) * 
-               B2^( coef[ "rho" ] / coef[ "rho_2" ] ) 
-         )^( - coef[ "nu" ] / coef[ "rho" ] - 1 ) *
-         ( ( 1 - coef[ "delta_3" ] ) * log( B2 ) * 
-            B2^( coef[ "rho" ] / coef[ "rho_2" ] ) *
-            ( - coef[ "rho" ] / coef[ "rho_2" ]^2 ) +
-            ( 1 - coef[ "delta_3" ] ) * ( coef[ "rho" ] / coef[ "rho_2" ] ) *
-            B2^( coef[ "rho" ] / coef[ "rho_2" ] - 1 ) *
-            ( -coef[ "delta_2" ] * log( data[[ xNames[ 3 ] ]] ) * 
-               data[[ xNames[ 3 ] ]]^( - coef[ "rho_2" ]) - 
-               ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 4 ] ]] ) * 
-               data[[ xNames[ 4 ] ]]^( - coef[ "rho_2" ] ) ) )
+      if( coef[ "rho_2" ] == 0 ) {
+         result <- - coef[ "gamma" ] * ( 1 - coef[ "delta_3" ] ) * coef[ "nu" ] *
+            ( ( 1 - coef[ "delta_3" ] ) * exp( coef[ "rho" ] * ( - BB2 ) ) +
+               coef[ "delta_3" ] * exp( coef[ "rho" ] * ( - BB1 ) )
+            )^( -coef[ "nu" ] / coef[ "rho" ] - 1 ) *
+            exp( - coef[ "rho" ] * BB2 ) *
+               ( - 0.5 * BB2^2 +
+                  0.5 * ( coef[ "delta_2" ] * log( data[[ xNames[ 3 ] ]] )^2 +
+                     ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 4 ] ]] )^2 )
+               )
+      } else {
+         result <- coef[ "gamma" ] * ( -coef[ "nu" ] / coef[ "rho" ] ) *
+            ( coef[ "delta_3" ] *
+               exp( coef[ "rho" ] * ( - BB1 ) ) +
+               ( 1 - coef[ "delta_3" ] ) * 
+                  B2^( coef[ "rho" ] / coef[ "rho_2" ] ) 
+            )^( - coef[ "nu" ] / coef[ "rho" ] - 1 ) *
+            ( ( 1 - coef[ "delta_3" ] ) * log( B2 ) * 
+               B2^( coef[ "rho" ] / coef[ "rho_2" ] ) *
+               ( - coef[ "rho" ] / coef[ "rho_2" ]^2 ) +
+               ( 1 - coef[ "delta_3" ] ) * ( coef[ "rho" ] / coef[ "rho_2" ] ) *
+               B2^( coef[ "rho" ] / coef[ "rho_2" ] - 1 ) *
+               ( -coef[ "delta_2" ] * log( data[[ xNames[ 3 ] ]] ) * 
+                  data[[ xNames[ 3 ] ]]^( - coef[ "rho_2" ]) - 
+                  ( 1 - coef[ "delta_2" ] ) * log( data[[ xNames[ 4 ] ]] ) * 
+                  data[[ xNames[ 4 ] ]]^( - coef[ "rho_2" ] ) ) )
+      }
    } else if( coef[ "rho_2" ] == 0 ) {
       result <- - coef[ "gamma" ] * coef[ "nu" ] * ( 1 - coef[ "delta_3" ] ) * 
          ( ( 1 - coef[ "delta_3" ] ) *
