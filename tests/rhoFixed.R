c4152a9c4d185f06a7c9593be5c56c6f6d5e0f57
Arne Henningsen <arne.henningsen@gmail.com>
2011-03-14 22:43:27 +0000

    added tests for 2-input CES with time effects (lambda) and at least one of the rhos fixed (before, this was tested only with the 3-input and 4-input nested CES functions)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1738 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- tests/rhoFixed.R
+++ tests/rhoFixed.R
@@ -7,6 +7,8 @@ data( germanFarms )
 germanFarms$qOutput <- germanFarms$vOutput / germanFarms$pOutput
 # quantity of intermediate inputs
 germanFarms$qVarInput <- germanFarms$vVarInput / germanFarms$pVarInput
+# time trend
+germanFarms$time <- c( 1:nrow( germanFarms ) )
 
 # names of explanatory variables
 xxNames <- c( "land", "qVarInput" )
@@ -31,6 +33,13 @@ coef( cesNmVrs )
 vcov( cesNmVrs )
 coef( summary( cesNmVrs ) )
 
+## Nelder-Mead, TC, VRS
+cesNmTcVrs <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   vrs = TRUE, rho = -0.4, method = "Nelder-Mead" )
+print.default( cesNmTcVrs )
+print( cesNmTcVrs )
+summary( cesNmTcVrs )
+
 ## Conjugate Gradients, CRS
 cesCg <- cesEst( "qOutput", xxNames, germanFarms, method = "CG", rho = 2 )
 print.default( cesCg )
@@ -39,6 +48,13 @@ summary( cesCg )
 coef( cesCg )
 vcov( cesCg )
 coef( summary( cesCg ) )
+
+## Conjugate Gradients, TC, CRS
+cesCgTc <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   method = "CG", rho = 0.2 )
+print.default( cesCgTc )
+print( cesCgTc )
+summary( cesCgTc )
 
 ## Conjugate Gradients, VRS
 cesCgVrs <- cesEst( "qOutput", xxNames, germanFarms, method = "CG", vrs = TRUE,
@@ -58,6 +74,13 @@ summary( cesSann )
 coef( cesSann )
 vcov( cesSann )
 coef( summary( cesSann ) )
+
+## Simulated Annealing, TC, CRS
+cesSannTc <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   method = "SANN", rho = 0 )
+print.default( cesSannTc )
+print( cesSannTc )
+summary( cesSannTc )
 
 ## Simulated Annealing, VRS
 cesSannVrs <- cesEst( "qOutput", xxNames, germanFarms, method = "SANN", vrs = TRUE,
@@ -88,6 +111,13 @@ coef( cesBfgsVrs )
 vcov( cesBfgsVrs )
 coef( summary( cesBfgsVrs ) )
 
+## BFGS, TC, VRS
+cesBfgsTcVrs <- cesEst( "qOutput", xxNames, tName = "time", 
+   data = germanFarms, method = "BFGS", vrs = TRUE, rho = -0.4 )
+print.default( cesBfgsTcVrs )
+print( cesBfgsTcVrs )
+summary( cesBfgsTcVrs )
+
 ## L-BFGS-B with constrained parameters, CRS
 cesBfgsCon <- cesEst( "qOutput", xxNames, germanFarms, method = "L-BFGS-B",
    rho = 2 )
@@ -97,6 +127,13 @@ summary( cesBfgsCon )
 coef( cesBfgsCon )
 vcov( cesBfgsCon )
 coef( summary( cesBfgsCon ) )
+
+## L-BFGS-B with constrained parameters, TC, CRS
+cesBfgsConTc <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   method = "L-BFGS-B", rho = 1.2 )
+print.default( cesBfgsConTc )
+print( cesBfgsConTc )
+summary( cesBfgsConTc )
 
 ## L-BFGS-B with constrained parameters, VRS
 cesBfgsConVrs <- cesEst( "qOutput", xxNames, germanFarms, method = "L-BFGS-B",
@@ -128,6 +165,13 @@ coef( cesLmVrs )
 vcov( cesLmVrs )
 coef( summary( cesLmVrs ) )
 
+## Levenberg-Marquardt, TC, VRS
+cesLmTcVrs <- cesEst( "qOutput", xxNames, tName = "time", germanFarms, 
+   vrs = TRUE, control = nls.lm.control( maxiter = 200 ), rho = 0.7 )
+print.default( cesLmTcVrs )
+print( cesLmTcVrs )
+summary( cesLmTcVrs )
+
 ## Newton-type, CRS
 cesNewton <- cesEst( "qOutput", xxNames, germanFarms, method = "Newton", rho = 2 )
 print.default( cesNewton )
@@ -136,6 +180,13 @@ summary( cesNewton )
 coef( cesNewton )
 vcov( cesNewton )
 coef( summary( cesNewton ) )
+
+## Newton-type, TC, CRS
+cesNewtonTc <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   method = "Newton", rho = 0.8 )
+print.default( cesNewtonTc )
+print( cesNewtonTc )
+summary( cesNewtonTc )
 
 ## Newton-type, VRS
 cesNewtonVrs <- cesEst( "qOutput", xxNames, germanFarms, method = "Newton",
@@ -166,6 +217,13 @@ coef( cesPortVrs )
 vcov( cesPortVrs )
 coef( summary( cesPortVrs ) )
 
+## PORT, TC, VRS
+cesPortTcVrs <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   method = "PORT", vrs = TRUE, rho = 0 )
+print.default( cesPortTcVrs )
+print( cesPortTcVrs )
+summary( cesPortTcVrs )
+
 ## DE, CRS
 cesDe <- cesEst( "qOutput", xxNames, germanFarms, method = "DE",
    control = DEoptim.control( trace = FALSE ), rho = 2 )
@@ -175,6 +233,13 @@ summary( cesDe )
 coef( cesDe )
 vcov( cesDe )
 coef( summary( cesDe ) )
+
+## DE, TC, CRS
+cesDeTc <- cesEst( "qOutput", xxNames, tName = "time", data = germanFarms, 
+   method = "DE", control = DEoptim.control( trace = FALSE ), rho = 0.5 )
+print.default( cesDeTc )
+print( cesDeTc )
+summary( cesDeTc )
 
 ## DE, VRS
 cesDeVrs <- cesEst( "qOutput", xxNames, germanFarms, method = "DE", vrs = TRUE,
