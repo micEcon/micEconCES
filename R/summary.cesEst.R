200d59d91c5b7d73edcb909149d1821e330db45d
Arne Henningsen <arne.henningsen@gmail.com>
2011-04-04 14:53:06 +0000

    corrected bug in summary.cesEst() that occurred in the calculation of the R-squared value if argument "rSquaredLog" was FALSE  and the model had an multiplicative error term, as well as if this argument was TRUE  and the model had an additive error term
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/micecon/pkg/micEconCES@1748 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- R/summary.cesEst.R
+++ R/summary.cesEst.R
@@ -7,14 +7,23 @@ summary.cesEst <- function( object, rSquaredLog = object$multErr, ... ) {
    object$sigma <- sqrt( object$rss / nObs )
 
    # R-squared value
+   fitVal <- fitted( object ) # the returned fitted values are never logged
    if( rSquaredLog ) {
-      object$r.squared <- rSquared( 
-         y = log( fitted( object ) ) + residuals( object ),
-         resid = residuals( object ) )
+      if( object$multErr ) {
+         yVal <- log( fitVal ) + residuals( object )  # log( y )
+      } else {
+         yVal <- log( fitVal + residuals( object ) )  # log( y )
+      }
+      res <- yVal - log( fitVal )
    } else {
-      object$r.squared <- rSquared( y = fitted( object ) + residuals( object ),
-         resid = residuals( object ) )
+      if( object$multErr ) {
+         yVal <- fitVal * exp( residuals( object ) )  # y
+      } else {
+         yVal <- fitVal + residuals( object )         # y
+      }
+      res <- yVal - fitVal
    }
+   object$r.squared <- rSquared( y = yVal, resid = res )
 
    # covariance matrix of the estimated coefficients/parameters
    if( is.null( object$vcov ) ) {
